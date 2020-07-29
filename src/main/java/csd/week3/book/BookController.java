40c3c1d61c387686472fd847747f6234d65ff0f8
Don Ta <donta@smu.edu.sg>
2020-07-29 13:19:56 +0800

    "Add HTTP status code for Post"

--- src/main/java/csd/week3/book/BookController.java
+++ src/main/java/csd/week3/book/BookController.java
@@ -2,12 +2,14 @@ package csd.week3.book;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -50,10 +52,12 @@ public class BookController {
     }
     /**
      * Add a new book with POST request to "/books"
+     * Note that HTTP status code should be 201 - Created
      * Note the use of @RequestBody
      * @param book
      * @return list of all books
      */
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/books")
     public Book addBook(@RequestBody Book book){
         return bookService.addBook(book);
