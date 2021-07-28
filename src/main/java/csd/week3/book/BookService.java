cf6abb9bf7e337046ce29b4c12f1ee3644c5392f
Don Ta <donta@smu.edu.sg>
2021-07-28 12:10:44 +0800

    Add comments for the service interface

--- src/main/java/csd/week3/book/BookService.java
+++ src/main/java/csd/week3/book/BookService.java
@@ -2,6 +2,10 @@ package csd.week3.book;
 
 import java.util.List;
 
+/**
+ * Here a service interface is declared to capture what are needed for the business logic of the application.
+ * We can provide different implementations of this interface later.
+  */
 public interface BookService {
     List<Book> listBooks();
     Book getBook(Long id);
